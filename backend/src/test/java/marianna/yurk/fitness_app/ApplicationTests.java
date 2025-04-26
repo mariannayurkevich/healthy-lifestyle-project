29d1c938542a83bc19715cc51904830f64e3be3c
Marianna <chockounicorn05@gmail.com>
2025-04-26 11:19:05 +0300

    fix: move to backend

--- backend/src/test/java/marianna/yurk/fitness_app/ApplicationTests.java
+++ backend/src/test/java/marianna/yurk/fitness_app/ApplicationTests.java
@@ -3,7 +3,6 @@ package marianna.yurk.fitness_app;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ApplicationTests {
 
 	@Test
