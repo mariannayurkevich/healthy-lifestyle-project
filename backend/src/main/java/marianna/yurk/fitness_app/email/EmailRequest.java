4940812851a56622b9842b90f051244d57915614
Marianna <chockounicorn05@gmail.com>
2025-04-30 20:28:20 +0300

    build: add EmailRequest.java

---
+++ backend/src/main/java/marianna/yurk/fitness_app/email/EmailRequest.java
@@ -0,0 +1,10 @@
+package marianna.yurk.fitness_app.email;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class EmailRequest {
+    private String email;
+}
