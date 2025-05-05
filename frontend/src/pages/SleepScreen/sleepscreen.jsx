60b12119907918fb6d3dcda8359e918453cf4255
MalinetskayaDiana <malinetskaya2004@gmail.com>
2025-05-06 02:36:20 +0300

    fixed sleep screen

--- frontend/src/pages/SleepScreen/sleepscreen.jsx
+++ frontend/src/pages/SleepScreen/sleepscreen.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { motion } from "framer-motion";
 import { useSwipeable } from "react-swipeable"; 
 import BackgroundGroup from "./components/background";
 import SleepText from "./components/goodnighttext";
@@ -25,29 +24,8 @@ export const SleepScreen = () => {
   const handleMenuClick4 = () => navigate("/account");
   const handleMenuClick5 = () => navigate("/sleep");
 
-  // Варианты анимации сдвига экрана:
-  const pageVariants = {
-    initial: { x: "100%", opacity: 0 },
-    animate: { x: "0%", opacity: 1 },
-    exit: { x: "-100%", opacity: 0 },
-  };
-
-  const pageTransition = {
-    type: "tween",
-    ease: "easeInOut",
-    duration: 0.7,
-  };
-
   return (
-    <motion.div
-      className="sleepscreen"
-      initial="initial"
-      animate="animate"
-      exit="exit"
-      variants={pageVariants}
-      transition={pageTransition}
-      {...swipeHandlers}
-    >
+    <div className="sleepscreen" {...swipeHandlers}>
       <BackgroundGroup />
       <SleepText />
       <SleepTimer />
@@ -58,6 +36,6 @@ export const SleepScreen = () => {
         onMenuClickStatistic={handleMenuClick2}
         onMenuClickChat={handleMenuClick3}
       />
-    </motion.div>
+    </div>
   );
 };
