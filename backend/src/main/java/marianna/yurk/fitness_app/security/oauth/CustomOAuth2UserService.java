88e4a71fca84bc10c77ea28e0bd0c9bc3a70196a
Marianna <chockounicorn05@gmail.com>
2025-05-06 08:14:51 +0300

    fix: update OAuthService

--- backend/src/main/java/marianna/yurk/fitness_app/security/oauth/CustomOAuth2UserService.java
+++ backend/src/main/java/marianna/yurk/fitness_app/security/oauth/CustomOAuth2UserService.java
@@ -1,5 +1,6 @@
 package marianna.yurk.fitness_app.security.oauth;
 
+import jakarta.transaction.Transactional;
 import marianna.yurk.fitness_app.user.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 import java.util.Map;
+import java.util.Optional;
 
 @Service
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
@@ -25,23 +27,40 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
     }
 
     @Override
+    @Transactional
     public OAuth2User loadUser(OAuth2UserRequest request) throws OAuth2AuthenticationException {
+        logger.info("Loading user from OAuth2 request");
         OAuth2User oAuth2User = super.loadUser(request);
 
         Map<String, Object> attributes = oAuth2User.getAttributes();
-        logger.info("All attributes from Google: {}", attributes);
-        String email = (String) attributes.get("email");
-        logger.info("Email from attributes: {}", email);
+        logger.info("All attributes: {}", attributes);
 
-        User user = userRepository.findByEmail(email).orElseGet(() -> {
+        String email = (String) attributes.get("email");
+        if (email == null) {
+            logger.error("Email not found in OAuth2 attributes");
+            throw new OAuth2AuthenticationException("Email not found");
+        }
+
+        logger.info("Looking for user with email: {}", email);
+        Optional<User> userOptional = userRepository.findByEmail(email);
+
+        // TODO: а нужна ли вообще строчка выше?
+
+        User user = userOptional.orElseGet(() -> {
+            logger.info("Creating new user for email: {}", email);
             User newUser = new User();
             newUser.setEmail(email);
-            newUser.setFirstName((String) attributes.get("name"));
+            newUser.setFirstName((String) attributes.get("given_name"));
+            newUser.setLastName((String) attributes.get("family_name"));
             newUser.setImageUrl((String) attributes.get("picture"));
+
             newUser.setProvider(AuthProvider.GOOGLE);
+            newUser.setEnabled(true);
+            newUser.setUserRole(UserRole.USER);
             return userRepository.save(newUser);
         });
 
+        logger.info("User found/created: {}", user);
         return new DefaultOAuth2User(
                 Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")),
                 attributes,
