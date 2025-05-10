8a39cd11ea6363028d9b593139b337ed7aab82db
Marianna <chockounicorn05@gmail.com>
2025-05-10 17:42:55 +0300

    fix: App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 // App.js
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route, useLocation, Navigate} from "react-router-dom";
 import { AnimatePresence, motion } from "framer-motion";
 
 // Импорты ваших страниц
@@ -17,31 +17,39 @@ import { QuestionnaireScreen } from "./pages/QuestionnaireScreen/questionnairesc
 import { WaterScreen } from "./pages/WaterScreen/waterscreen";
 import { ActivityScreen } from "./pages/ActivityScreen/activityscreen";
 import { FoodScreen } from "./pages/FoodScreen/foodscreen";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 // Оборачиваем маршруты в компонент с анимацией
 const AnimatedRoutes = () => {
-  const location = useLocation();
   return (
-    <AnimatePresence exitBeforeEnter>
-      <Routes location={location} key={location.pathname}>
-        <Route path="/" element={<FirstScreen />} />
-        <Route path="/main" element={<MainScreen />} />
-        <Route path="/sleep" element={<SleepScreen />} />
-        <Route path="/entry" element={<EntryScreen />} />
-        <Route path="/first" element={<FirstScreen />} />
-        <Route path="/registrationfirst" element={<RegistrationFirstScreen />} />
-        <Route path="/registrationsecond" element={<RegistrationSecondScreen />} />
-        <Route path="/statistic" element={<StatisticScreen />} />
-        <Route path="/chat" element={<ChatScreen />} />
-        <Route path="/account" element={<AccountScreen />} />
-        <Route path="/water" element={<WaterScreen />} />
-        <Route path="/activity" element={<ActivityScreen />} />
-        <Route path="/food" element={<FoodScreen />} />
-        <Route path="/questionnaire" element={<QuestionnaireScreen />} />
-      </Routes>
-    </AnimatePresence>
+      <AnimatePresence exitBeforeEnter>
+        <Routes>
+          {/* Открытые маршруты */}
+          <Route path="/" element={<FirstScreen />} />
+          <Route path="/entry" element={<EntryScreen />} />
+          <Route path="/registrationfirst" element={<RegistrationFirstScreen />} />
+          <Route path="/registrationsecond" element={<RegistrationSecondScreen />} />
+
+          {/* Защищенные маршруты */}
+          <Route element={<ProtectedRoute />}>
+            <Route path="/main" element={<MainScreen />} />
+            <Route path="/sleep" element={<SleepScreen />} />
+            <Route path="/statistic" element={<StatisticScreen />} />
+            <Route path="/chat" element={<ChatScreen />} />
+            <Route path="/account" element={<AccountScreen />} />
+            <Route path="/water" element={<WaterScreen />} />
+            <Route path="/activity" element={<ActivityScreen />} />
+            <Route path="/food" element={<FoodScreen />} />
+            <Route path="/questionnaire" element={<QuestionnaireScreen />} />
+          </Route>
+
+          {/* Резервный маршрут */}
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </AnimatePresence>
   );
 };
+
 
 const RoutesWrapper = () => {
   return <AnimatedRoutes />;
